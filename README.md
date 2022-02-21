840268bece2faa9b0731013641ed2622a2c940ce
Min <mjwithu09@gmail.com>
2022-02-21 13:22:52 -0800

    ES6

---
+++ README.md
@@ -0,0 +1,5 @@
+# Project Set up base on Web Dev simplified
+https://www.youtube.com/watch?v=qv24S2L1N0k&t=285s
+
+# 
+Heroku 
--- bot.js
+++ bot.js
@@ -1,17 +1,16 @@
-require("dotenv").config()
+import 'dotenv/config' // ES6
+import Discord from "discord.js";
 
-const Discord = require('discord.js');
 const client = new Discord.Client({intents:["GUILDS","GUILD_MESSAGES"]});
 
 client.on('ready',()=>{
     console.log('Out Bot is ready to go')
 })
 
-
 client.on("message", msg =>{
     if(msg.content ==="Hey"){
       msg.reply("Helllooo")
     }
   })
-  
+
 client.login(process.env.BOT_TOKEN)
