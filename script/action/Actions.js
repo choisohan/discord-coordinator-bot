4f8df1ab7722e9ecba73f5ef32f73bf29fe00b04
Min <mjwithu09@gmail.com>
2022-03-11 15:29:45 -0800

    puppeteer remove timeout

--- script/action/Actions.js
+++ script/action/Actions.js
@@ -726,14 +726,18 @@ export async function TellMeAboutSocialStat(_entitie){
     
         var URL = 'https://www.instagram.com/happping_min/'
         console.log("💛URL", URL )
-        await page.goto(URL, {waitUntil: 'networkidle2'});
+        await page.goto(URL, {
+            waitUntil: "load",timeout: 0
+        });
         await page.waitForSelector('.Y8-fY'); 
         stats.instagram = await page.$$eval('.Y8-fY', els => els.map(el => el.textContent ) ); //posts, followers, following
         stats.instagram = stats.instagram[1];
         
         var URL = 'https://twitter.com/happping_min'
         console.log("💛URL", URL )
-        await page.goto(URL, {waitUntil: 'networkidle2'});
+        await page.goto(URL, {
+            waitUntil: "load",timeout: 0
+        });
         await page.waitForSelector('.css-4rbku'); 
         stats.twitter = await page.$$eval('.css-4rbku5', els => els.map(el => el.textContent ).filter(el => el.includes("Followers") ) );
         stats.twitter = stats.twitter[0]
